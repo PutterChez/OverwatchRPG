e967ef759743f54e49a231fc34d3530a945a7bb8
DiaGreine <44259480+DiaGreine@users.noreply.github.com>
2018-10-18 19:55:03 +0900

    UpdateSkillClass
    randomHitRate and Stuff

--- GameTest/src/gametest/Action.java
+++ GameTest/src/gametest/Action.java
@@ -1,10 +1,23 @@
 package gametest;
+import java.util.*;
 
 public class Action {
     public static void attack(Entity attacker, Skill attackerSkill,  Entity target)
     {
         //Example
-        target.HP -= (attacker.attack * attackerSkill.getAtkPower());
-        attacker.MP -= attackerSkill.mpCost;
+        Random randHit = new Random(System.currentTimeMillis());
+        int attackHitRate = attackerSkill.accuracy - target.evasion;
+
+        if (randHit.nextInt(100) < attackHitRate)
+        {
+            //Hit
+            target.HP -= (attacker.attack * attackerSkill.getAtkPower());
+            attacker.MP -= attackerSkill.mpCost;
+        }
+        else
+        {
+            //Miss
+            //Do something IDK
+        }
     }
 }

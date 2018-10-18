2d37a9189cbfe60aa47c0d2b6b53b480bfd1d725
DiaGreine <44259480+DiaGreine@users.noreply.github.com>
2018-10-18 19:23:12 +0900

    skillUpdate

--- GameTest/src/gametest/Skill.java
+++ GameTest/src/gametest/Skill.java
@@ -1,11 +1,59 @@
 package gametest;
 
 public class Skill {
-    String name;
-    public Skill(String name){
-        this.name = name;
+    private String skillName;
+    private double atkPower; // Percentage
+    private int mpCost;
+    private int accuracy;
+    private String description = "";
+
+    public Skill(String name, double atkPower, int mpCost, int accuracy){
+        this.skillName = name;
+        this.atkPower = atkPower;
+        this.mpCost = mpCost;
+        this.accuracy = accuracy;
     }
-    
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+
+    public double getAtkPower() {
+        return atkPower;
+    }
+
+    public void setAtkPower(double atkPower) {
+        this.atkPower = atkPower;
+    }
+
+    public int getMpCost() {
+        return mpCost;
+    }
+
+    public void setMpCost(int mpCost) {
+        this.mpCost = mpCost;
+    }
+
+    public int getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(int accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void act(){
         
     }

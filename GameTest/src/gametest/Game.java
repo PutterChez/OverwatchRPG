a244e1b86dfd52c0aaf43cbc6476a3d62e6aa747
PutterChez <44222382+PutterChez@users.noreply.github.com>
2018-10-24 14:08:47 +0700

    FIX

--- GameTest/src/gametest/Game.java
+++ GameTest/src/gametest/Game.java
@@ -33,7 +33,6 @@ public class Game extends Canvas implements Runnable {
         posX3 = WIDTH/2 + 400; posY3 = HEIGHT/2 - 300;
         posX4 = WIDTH/2 - 50; posY4 = HEIGHT/2 - 300;
         
-<<<<<<< HEAD
         int enemyX1, enemyX2, enemyX3, enemyX4, enemyX5, enemyX6, enemyY1, enemyY2, enemyY3, enemyY4, enemyY5, enemyY6;
         enemyX1 = WIDTH/2 - 800; enemyY1 = HEIGHT/2 - 500;
         enemyX2 = WIDTH/2 - 800; enemyY2 = HEIGHT/2 - 300;
@@ -41,18 +40,11 @@ public class Game extends Canvas implements Runnable {
         enemyX4 = WIDTH/2 - 300; enemyY4 = HEIGHT/2 - 500;
         enemyX5 = WIDTH/2 - 300; enemyY5 = HEIGHT/2 - 500;
         enemyX6 = WIDTH/2 - 300; enemyY6 = HEIGHT/2 - 500;
-=======
+
         Entity genji = new Entity(posX2,posY2, ID.Genji, 400, 400, "..//resources//characters//genji_1.png", 200, 100, "Genji");
-        Entity doomfist = new Entity(posX2,posY2, ID.Doom, 300, 300, "C:\\Users\\PRO_10\\Documents\\GitHub\\OverwatchRPG\\resources\\characters\\doom_1.png", 250, 200, "Doomfist");
+        Entity mccree = new Entity(posX1,posY1, ID.Doom, 400, 400, "C:\\Users\\PRO_10\\Documents\\GitHub\\OverwatchRPG\\resources\\characters\\mccree_1.png", 250, 200, "Mccree");
         Entity mercy = new Entity(posX3,posY3, ID.Mercy, 400, 400, "C:\\Users\\PRO_10\\Documents\\GitHub\\OverwatchRPG\\resources\\characters\\mercy_1.png", 200, 150, "Mercy");
         Entity reinhardt = new Entity(posX4,posY4, ID.Rein, 350, 350, "C:\\Users\\PRO_10\\Documents\\GitHub\\OverwatchRPG\\resources\\characters\\rein_1.png", 500, 150, "Reinhardt");
->>>>>>> 88e341f2546dd51f51a8d62e9a9629b72a80d083
-        
-        
-        Entity genji = new Entity(posX2,posY2, ID.Genji, 400, 400, "..\\resources\\characters\\genji_1.png", 200, 100, "Genji");
-        Entity mercy = new Entity(posX3,posY3, ID.Mercy, 400, 400, "..\\resources\\characters\\mercy_1.png", 200, 150, "Mercy");
-        Entity reinhardt = new Entity(posX4,posY4, ID.Rein, 350, 350, "..\\resources\\characters\\rein_1.png", 500, 150, "Reinhardt");
-        Entity mccree = new Entity(posX1,posY1, ID.Mccree, 400, 400, "..\\resources\\characters\\mccree_1.png", 200, 100, "Mccree");
         
         Entity doomfist = new Entity(enemyX1, enemyY1, ID.Doom, 300, 300, "..\\resources\\characters\\doom_2.png", 250, 200, "Doomfist");
         Entity widowmaker = new Entity(enemyX2, enemyY2, ID.Widow, 300, 300, "..\\resources\\characters\\widow_2.png", 250, 200, "Widowmaker");

d7348b5b46aee4eefdc767045e6cd2f34a75eab3
DiaGreine <44259480+DiaGreine@users.noreply.github.com>
2018-10-24 15:52:22 +0900

    Party

---
+++ GameTest/src/gametest/Party.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gametest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author DELL
+ */
+public class Party {
+    protected List<PartyMember> memberList;
+    
+    Party()
+    {
+        ArrayList memberList = new ArrayList(){};
+    }
+    
+    
+    
+}

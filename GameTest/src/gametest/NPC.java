24e2076860aa9edfdcfd1329d33f8ecf81947a93
DiaGreine <44259480+DiaGreine@users.noreply.github.com>
2018-11-22 00:01:56 +0900

    Nevermind the Last one

--- GameTest/src/gametest/NPC.java
+++ GameTest/src/gametest/NPC.java
@@ -9,6 +9,23 @@ package gametest;
  *
  * @author DELL
  */
-public class NPC {
+public class NPC extends WorldPhaseEntity{
+    String dialogue;
+    
+    NPC(int x, int y, ID id, int width, int height, String imageDirectory, String charName)
+    {
+        super(x, y, id, width, height, imageDirectory, charName);
+    }
+    
+    protected void setDialogue(String script)
+    {
+        dialogue = script;
+    }
+    
+    public void interacted()
+    {
+        //Pop-Up here
+        System.out.println(dialogue);
+    }
     
 }
